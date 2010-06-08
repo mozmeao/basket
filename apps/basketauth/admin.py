d00822a84b38a0a30a7fec287e81271df482c903
Alex Buchanan <abuchanan@mozilla.com>
2010-06-08 12:39:15 -0700

    add status to consumer admin, to try and debug stage issue with auth

--- apps/basketauth/admin.py
+++ apps/basketauth/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 
 class ConsumerAdmin(admin.ModelAdmin):
-    fields = ('name', 'key', 'secret')
+    fields = ('name', 'key', 'secret', 'status')
     readonly_fields = ('key', 'secret')
 
     def save_model(self, request, obj, form, change):
