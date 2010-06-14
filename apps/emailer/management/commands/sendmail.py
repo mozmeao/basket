aa2c67ee0853fe71817b6b0d1432edded2fb18e1
Fred Wenzel <fwenzel@mozilla.com>
2010-06-14 18:56:30 +0200

    make sure the same sendmail command is not run twice at the same time by using a lockfile.

--- apps/emailer/management/commands/sendmail.py
+++ apps/emailer/management/commands/sendmail.py
@@ -1,9 +1,44 @@
+import functools
+import logging
 from optparse import make_option
+import os
+import tempfile
+
+import lockfile
 
 from django.core.management.base import LabelCommand, CommandError
 from django.core.urlresolvers import get_callable
 
 from emailer.models import Email
+
+
+LOCKFILE_PREFIX = 'basket_emailer_lock'
+
+
+def locked(f):
+    """
+    Decorator that only allows one instance of the same sendmail command to run
+    at a time.
+    """
+    @functools.wraps(f)
+    def wrapper(self, label, **options):
+        name = '_'.join((LOCKFILE_PREFIX, f.__name__, options.get(
+            'template', None), label))
+        file = os.path.join(tempfile.gettempdir(), name)
+        lock = lockfile.FileLock(file)
+        try:
+            # Try to acquire the lock without blocking.
+            lock.acquire(0)
+        except lockfile.LockError:
+            logging.debug('Aborting %s; lock acquisition failed.' % name)
+            return 0
+        else:
+            # We have the lock, call the function.
+            try:
+                return f(self, label, **options)
+            finally:
+                lock.release()
+    return wrapper
 
 
 class Command(LabelCommand):
@@ -17,7 +52,12 @@ class Command(LabelCommand):
     args='<campaign campaign ...>'
     label='campaign'
 
+    @locked
     def handle_label(self, label, **options):
+        """
+        Locked command handler to avoid running this command more than once
+        simultaneously.
+        """
         template = getattr(self, 'template', None)
         if not template:
             template_name = options.get('template', None)
