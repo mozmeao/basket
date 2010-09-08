713e3970c58934919cc8faf67a09ddfd0a98408a
Alex Buchanan <abuchanan@mozilla.com>
2010-09-08 15:21:28 -0700

    CSRF exempt piston resource

--- apps/subscriptions/urls.py
+++ apps/subscriptions/urls.py
@@ -1,14 +1,17 @@
 from django.conf.urls.defaults import *
 
-from piston.resource import Resource
-
 from piston.authentication import NoAuthentication
+
+from csrf_exempt_resource import CsrfExemptResource
 from .handlers import SubscriptionHandler
+
 
 auth = NoAuthentication()
 
-subscribe = Resource(handler=SubscriptionHandler, authentication=auth)
+subscribe = CsrfExemptResource(handler=SubscriptionHandler, authentication=auth)
 
 urlpatterns = patterns('',
     url('^subscribe/$', subscribe, name='subscriptions.subscribe'),
 )
+
+
---
+++ libs/csrf_exempt_resource.py
@@ -0,0 +1,8 @@
+from piston.resource import Resource
+
+
+class CsrfExemptResource(Resource):
+    """A Custom Resource that is csrf exempt"""
+    def __init__(self, handler, authentication=None):
+        super(CsrfExemptResource, self).__init__(handler, authentication)
+        self.csrf_exempt = getattr(self.handler, 'csrf_exempt', True)
