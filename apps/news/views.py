025c74609397731a7df4e9fc018d83459d7bcc8e
James Long <jlongster@jlongster.com>
2011-10-28 19:09:11 -0400

    fix check for triggering welcome email

--- apps/news/views.py
+++ apps/news/views.py
@@ -239,7 +239,7 @@ def update_user(request, type, data=None):
                               record.keys(),
                               record.values())
 
-        if 'trigger_welcome' in data:
+        if int(data.get('trigger_welcome', False)):
             rs.trigger_custom_event(record['EMAIL_ADDRESS_'],
                                     settings.RESPONSYS_FOLDER,
                                     settings.RESPONSYS_LIST,
