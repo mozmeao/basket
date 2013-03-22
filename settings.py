e2f16c407319d6b617f5af9b2661084401664215
Paul McLanahan <paul@mclanahan.net>
2013-03-22 11:25:11 -0400

    Fix deprecation error and add 500 template.

--- settings.py
+++ settings.py
@@ -54,7 +54,7 @@ TEMPLATE_LOADERS = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
     'csrf_context.csrf',
 )
