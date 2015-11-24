99592f5474726545eb0c5cba366a92285d545a3a
Giorgos Logiotatidis <seadog@sealabs.net>
2015-11-24 20:14:47 +0200

    Add commit SHA to static/revision.txt

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -4,4 +4,6 @@
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
+echo "$GIT_SHA" > static/revision.txt
+
 exec gunicorn wsgi:application -b 0.0.0.0:8000 -w 2 --log-file -
