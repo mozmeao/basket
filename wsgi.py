d755537e0a72a87df7b62232b066d17871c12091
Paul McLanahan <pmac@mozilla.com>
2015-11-24 10:18:32 -0500

    Add newrelic to wsgi file.

--- wsgi.py
+++ wsgi.py
@@ -6,6 +6,21 @@ from django.core.wsgi import get_wsgi_application
 
 from whitenoise.django import DjangoWhiteNoise
 
+try:
+    import newrelic.agent
+except ImportError:
+    newrelic = False
+
+
+if newrelic:
+    newrelic_ini = os.getenv('NEWRELIC_INI_FILE', False)
+    if newrelic_ini:
+        newrelic.agent.initialize(newrelic_ini)
+    else:
+        newrelic = False
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+
+if newrelic:
+    application = newrelic.agent.wsgi_application()(application)
