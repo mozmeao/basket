b5bd05b1284d02dad1c84c37fdcdf7c42f8b1e5f
Paul McLanahan <pmac@mozilla.com>
2016-01-20 10:17:20 -0500

    Skip DB migration in RO mode

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -2,7 +2,11 @@
 
 # need to do this here because docker build has no env
 python manage.py collectstatic --noinput
-python manage.py migrate --noinput
+
+READ_ONLY_MODE=$(echo "$READ_ONLY_MODE" | tr '[:upper:]' '[:lower:]')
+if [[ "$READ_ONLY_MODE" != "true" ]]; then
+    python manage.py migrate --noinput
+fi
 
 echo "$GIT_SHA" > static/revision.txt
 
