9532dc20f7f04ddf09143c216d650e9cbf0ca113
Paul McLanahan <pmac@mozilla.com>
2016-01-26 11:29:55 -0500

    Switch to moztravis docker hub account

--- bin/deploy-travis.sh
+++ bin/deploy-travis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -e
+set -ex
 
 docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker push ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT}
-docker tag -f ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} ${DOCKER_REPOSITORY}:last_successful_build
+docker push ${DOCKER_IMAGE_TAG}
+docker tag -f ${DOCKER_IMAGE_TAG} ${DOCKER_REPOSITORY}:last_successful_build
 docker push ${DOCKER_REPOSITORY}:last_successful_build
 
 # Install deis client
@@ -15,7 +15,7 @@ for region in us-west eu-west; do
     DEIS_CONTROLLER=https://deis.${region}.moz.works
     NR_APP="${DEIS_APP}-${region}"
     ./deis login $DEIS_CONTROLLER  --username $DEIS_USERNAME --password $DEIS_PASSWORD
-    ./deis pull ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} -a $DEIS_APP
+    ./deis pull ${DOCKER_IMAGE_TAG} -a $DEIS_APP
     curl -H "x-api-key:$NEWRELIC_API_KEY" \
          -d "deployment[app_name]=$NR_APP" \
          -d "deployment[revision]=$TRAVIS_COMMIT" \
