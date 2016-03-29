7e4869e408a7bc2a547c40f6df3e6573da8def9e
Paul McLanahan <pmac@mozilla.com>
2016-03-28 21:57:06 -0400

    Add SMTP email settings

--- settings.py
+++ settings.py
@@ -68,6 +68,16 @@ CACHES = {
     },
 }
 
+default_email_backend = ('django.core.mail.backends.console.EmailBackend' if DEBUG else
+                         'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_BACKEND = config('EMAIL_BACKEND', default=default_email_backend)
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_SUBJECT_PREFIX = config('EMAIL_SUBJECT_PREFIX', default='[basket] ')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+
 ALLOWED_HOSTS = config('ALLOWED_HOSTS',
                        default='.allizom.org, basket.mozilla.com, basket.mozilla.org',
                        cast=Csv())
