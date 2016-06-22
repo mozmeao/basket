2200c1e1684c1059e9884bc36c43b615ab33232f
Paul McLanahan <pmac@mozilla.com>
2016-06-22 11:48:39 -0400

    Change default last name back to _

--- news/backends/sfdc.py
+++ news/backends/sfdc.py
@@ -12,7 +12,7 @@ from news.newsletters import newsletter_map, newsletter_inv_map
 
 
 time_request = get_timer_decorator('news.backends.sfdc')
-LAST_NAME_DEFAULT_VALUE = ','
+LAST_NAME_DEFAULT_VALUE = '_'
 FIELD_MAP = {
     'id': 'Id',
     'email': 'Email',
