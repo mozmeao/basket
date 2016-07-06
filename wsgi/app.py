f35f22cc98e7cda5377168e5893d2377899f9c69
Paul McLanahan <pmac@mozilla.com>
2016-07-06 08:32:52 -0400

    Add Sentry WSGI middleware

--- wsgi/app.py
+++ wsgi/app.py
@@ -4,6 +4,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
 
+from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 from whitenoise.django import DjangoWhiteNoise
 
 try:
@@ -21,6 +22,7 @@ if newrelic:
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+application = Sentry(application)
 
 if newrelic:
     application = newrelic.agent.WSGIApplicationWrapper(application)
