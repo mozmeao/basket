b5dc1bfc201d5c8118d451860e01a12ebf25e8d4
Paul McLanahan <pmac@mozilla.com>
2016-07-06 11:44:28 -0400

    Move wsgi config into run-prod.sh

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 echo "$GIT_SHA" > static/revision.txt
-exec gunicorn wsgi.app --config wsgi/config.py
+exec gunicorn wsgi.app --bind "0.0.0.0:${PORT:-8000}" --error-logfile - --access-logfile - \
+                       --workers "${WSGI_NUM_WORKERS:-2}" \
+                       --worker-class "${WSGI_WORKER_CLASS:-sync}" \
+                       --log-level "${WSGI_LOG_LEVEL:-warning}"
--- wsgi/config.py
+++
@@ -1,9 +0,0 @@
-# see http://docs.gunicorn.org/en/latest/configure.html#configuration-file
-
-from os import getenv
-
-
-bind = '0.0.0.0:8000'
-workers = getenv('WSGI_NUM_WORKERS', 2)
-errorlog = '-'
-loglevel = getenv('WSGI_LOG_LEVEL', 'warning')
