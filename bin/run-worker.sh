688f167826abd869d5b3706472530364514cf889
Paul McLanahan <pmac@mozilla.com>
2016-07-06 10:31:29 -0400

    Enable ability to use gevent celery pool
    Configurable via CELERY_POOL environment variable.

--- bin/run-worker.sh
+++ bin/run-worker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 exec newrelic-admin run-program celery -A news worker \
+                                       -P "${CELERY_POOL:-prefork}" \
                                        -l "${CELERY_LOG_LEVEL:-warning}" \
                                        -c "${CELERY_NUM_WORKERS:-4}"
