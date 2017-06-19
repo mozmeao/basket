06be735cbffa98b85b1d81f01cfd8bb4315043b6
Paul McLanahan <pmac@mozilla.com>
2017-06-19 19:49:46 -0400

    Fix run-workers command

--- bin/run-worker.sh
+++ bin/run-worker.sh
@@ -3,5 +3,5 @@
 exec newrelic-admin run-program celery -A news worker \
                                        -P "${CELERY_POOL:-prefork}" \
                                        -l "${CELERY_LOG_LEVEL:-warning}" \
-                                       -c "${CELERY_NUM_WORKERS:-4}"
+                                       -c "${CELERY_NUM_WORKERS:-4}" \
                                        -Q celery,snitch
