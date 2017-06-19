274e7ec6709c8ac38cb92d73411f3a0329e8af1e
Paul McLanahan <pmac@mozilla.com>
2017-06-19 19:13:52 -0400

    Workers should consume snitch queue

--- bin/run-worker.sh
+++ bin/run-worker.sh
@@ -4,3 +4,4 @@ exec newrelic-admin run-program celery -A news worker \
                                        -P "${CELERY_POOL:-prefork}" \
                                        -l "${CELERY_LOG_LEVEL:-warning}" \
                                        -c "${CELERY_NUM_WORKERS:-4}"
+                                       -Q celery,snitch
