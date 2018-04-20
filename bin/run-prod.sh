c4adbf2a47a66224f87a23809a1ab1f3bceb2dc2
Paul McLanahan <pmac@mozilla.com>
2018-04-20 13:39:02 -0400

    Add Meinheld WSGI Worker dependencies

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -3,7 +3,7 @@
 echo "$GIT_SHA" > static/revision.txt
 exec gunicorn basket.wsgi --bind "0.0.0.0:${PORT:-8000}" \
                           --workers "${WSGI_NUM_WORKERS:-2}" \
-                          --worker-class "${WSGI_WORKER_CLASS:-sync}" \
+                          --worker-class "${WSGI_WORKER_CLASS:-meinheld.gmeinheld.MeinheldWorker}" \
                           --log-level "${WSGI_LOG_LEVEL:-warning}" \
                           --error-logfile - \
                           --access-logfile -
