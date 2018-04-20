0406012775f39366506733cdfed3fdd5956afa48
Paul McLanahan <pmac@mozilla.com>
2018-04-20 17:01:37 -0400

    Upgrade Whitenoise and Gunicorn
    Also set gunicorn log level to "info" so we can see versions in the log

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -4,6 +4,6 @@ echo "$GIT_SHA" > static/revision.txt
 exec gunicorn basket.wsgi --bind "0.0.0.0:${PORT:-8000}" \
                           --workers "${WSGI_NUM_WORKERS:-8}" \
                           --worker-class "${WSGI_WORKER_CLASS:-meinheld.gmeinheld.MeinheldWorker}" \
-                          --log-level "${WSGI_LOG_LEVEL:-warning}" \
+                          --log-level "${WSGI_LOG_LEVEL:-info}" \
                           --error-logfile - \
                           --access-logfile -
