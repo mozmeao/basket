74e4f48d05158eb4bc35890e079cdaed0586b19e
Paul McLanahan <pmac@mozilla.com>
2018-04-20 14:11:33 -0400

    Increase default WSGI worker count to 8

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -2,7 +2,7 @@
 
 echo "$GIT_SHA" > static/revision.txt
 exec gunicorn basket.wsgi --bind "0.0.0.0:${PORT:-8000}" \
-                          --workers "${WSGI_NUM_WORKERS:-2}" \
+                          --workers "${WSGI_NUM_WORKERS:-8}" \
                           --worker-class "${WSGI_WORKER_CLASS:-meinheld.gmeinheld.MeinheldWorker}" \
                           --log-level "${WSGI_LOG_LEVEL:-warning}" \
                           --error-logfile - \
