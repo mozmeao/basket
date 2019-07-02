65e3d94d9ab2e2970019f3cf960a0971b3ce9811
Paul McLanahan <pmac@users.noreply.github.com>
2019-07-02 10:38:36 -0400

    Upgrade Django to 2.2.3 (#200)
    Fix #97

--- basket/urls.py
+++ basket/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 from django.views.generic import RedirectView, TemplateView
 
@@ -9,30 +9,32 @@ from basket.news.views import subscribe_main, subscribe_json, fxa_start, fxa_cal
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='home.html')),
-    url(r'^watchman/$', watchman_views.dashboard, name="watchman.dashboard"),
-    url(r'^healthz/$', watchman_views.ping, name="watchman.ping"),
-    url(r'^readiness/$', watchman_views.status, name="watchman.status"),
+    path('', TemplateView.as_view(template_name='home.html')),
+    path('watchman/', watchman_views.dashboard, name="watchman.dashboard"),
+    path('healthz/', watchman_views.ping, name="watchman.ping"),
+    path('readiness/', watchman_views.status, name="watchman.status"),
 ]
 
 if not settings.ADMIN_ONLY_MODE:
-    urlpatterns.append(url(r'^news/', include('basket.news.urls')))
-    urlpatterns.append(url(r'^subscribe/?$', subscribe_main))
-    urlpatterns.append(url(r'^subscribe\.json$', subscribe_json))
-    urlpatterns.append(url(r'^fxa/?$', fxa_start))
-    urlpatterns.append(url(r'^fxa/callback/?$', fxa_callback))
-    urlpatterns.append(url(r'^subhub/?$', subhub_post))
+    urlpatterns.extend([
+        path('news/', include('basket.news.urls')),
+        path('subscribe/', subscribe_main),
+        path('subscribe.json', subscribe_json),
+        path('fxa/', fxa_start),
+        path('fxa/callback/', fxa_callback),
+        path('subhub/', subhub_post),
+    ])
 
 if settings.DISABLE_ADMIN:
     urlpatterns.append(
-        url(r'^admin/', RedirectView.as_view(url=settings.ADMIN_REDIRECT_URL, permanent=True))
+        path('admin/', RedirectView.as_view(url=settings.ADMIN_REDIRECT_URL, permanent=True))
     )
 else:
     if settings.OIDC_ENABLE:
-        urlpatterns.append(url(r'^oidc/', include('mozilla_django_oidc.urls')))
+        urlpatterns.append(path('oidc/', include('mozilla_django_oidc.urls')))
 
     admin.autodiscover()
     urlpatterns.extend([
-        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-        url(r'^admin/', include(admin.site.urls)),
+        path('admin/doc/', include('django.contrib.admindocs.urls')),
+        path('admin/', admin.site.urls),
     ])
