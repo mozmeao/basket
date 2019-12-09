e09fc9015757de597c9cab1643a4fe5c6aed7310
Paul McLanahan <pmac@mozilla.com>
2019-12-09 11:54:10 -0500

    Fix run-prod script errors
    Used exec command in the wrong place.

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 echo "$GIT_SHA" > static/revision.txt
-exec NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program \
+NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program \
 gunicorn basket.wsgi --bind "0.0.0.0:${PORT:-8000}" \
                      --workers "${WSGI_NUM_WORKERS:-8}" \
                      --worker-class "${WSGI_WORKER_CLASS:-meinheld.gmeinheld.MeinheldWorker}" \
