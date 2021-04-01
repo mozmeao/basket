c072d9a4295f520f25c470c8d047a4f8bd233300
John Whitlock <jwhitlock@mozilla.com>
2021-04-01 16:07:26 -0500

    Add CTMSInterface class
    CTMSInterface has methods for each of the public CTMS API calls.
    
    It is difficult to test at this level without a lot of mocking or
    simulating CTMS, and I tried a few approaches that made the testing code
    as complex as the application code.
    
    mock_interface() is a compromise, which will allow mocking of successful
    responses as well as errors. Integrated testing will be needed to fully
    test the code.

--- basket/news/backends/ctms.py
+++ basket/news/backends/ctms.py
@@ -12,6 +12,10 @@ from django.core.cache import cache
 from requests_oauthlib import OAuth2Session
 from oauthlib.oauth2 import BackendApplicationClient
 
+from basket.news.backends.common import get_timer_decorator
+
+time_request = get_timer_decorator("news.backends.ctms")
+
 
 class CTMSSession:
     """Add authentication to requests to the CTMS API"""
@@ -110,6 +114,120 @@ class CTMSSession:
     put = partialmethod(request, "PUT")
 
 
+class CTMSInterface:
+    """Basic Interface to the CTMS API"""
+
+    # Identifiers that uniquely identity a CTMS record
+    unique_ids = [
+        "email_id",
+        "basket_token",
+        "primary_email",
+        "sfdc_id",
+        "fxa_id",
+        "mofo_email_id",
+    ]
+    # Identifiers that can be shared by multiple CTMS records
+    shared_ids = ["mofo_contact_id", "amo_user_id", "fxa_primary_email"]
+    all_ids = unique_ids + shared_ids
+
+    def __init__(self, session):
+        self.session = session
+
+    @time_request
+    def get_by_alternate_id(
+        self,
+        email_id=None,
+        primary_email=None,
+        basket_token=None,
+        sfdc_id=None,
+        fxa_id=None,
+        mofo_email_id=None,
+        mofo_contact_id=None,
+        amo_user_id=None,
+        fxa_primary_email=None,
+    ):
+        """
+        Call GET /ctms to get a list of contacts matching all alternate IDs.
+
+        @param email_id: CTMS email ID
+        @param primary_email: User's primary email
+        @param basket_token: Basket's token
+        @param sfdc_id: Legacy SalesForce.com ID
+        @param fxa_id: Firefox Accounts ID
+        @param mofo_email_id: Mozilla Foundation email ID
+        @param mofo_contact_id: Mozilla Foundation contact ID
+        @param amo_user_id: User ID from addons.mozilla.org
+        @param fxa_primary_email: Primary email in Firefox Accounts
+        @return: list of contact dicts
+        @raises: request.HTTPError, status_code 400, on bad auth credentials
+        @raises: request.HTTPError, status_code 401, on no auth credentials
+        @raises: request.HTTPError, status_code 404, on unknown email_id
+        """
+
+        ids = {}
+        for name, value in locals().items():
+            if name in self.all_ids and value is not None:
+                ids[name] = value
+        if not ids:
+            raise ValueError("At least one ID must be non-null")
+        resp = self.session.get("/ctms", params=ids)
+        resp.raise_for_status()
+        return resp.json()
+
+    @time_request
+    def post_to_create(self, data):
+        """
+        Call POST /ctms to create a contact.
+
+        The email (email.primary_email) is the only required item. If the CTMS
+        email_id is not set (email.email_id), the server will generate one. Any
+        unspecified value will be set to a default.
+
+        @param data: The contact data, as a nested dictionary
+        @return: The created contact data
+        """
+        resp = self.session.post("/ctms", json=data)
+        resp.raise_for_status()
+        return resp.json()
+
+    @time_request
+    def get_by_email_id(self, email_id):
+        """
+        Call GET /ctms/{email_id} to get contact data by CTMS email_id.
+
+        @param email_id: The CTMS email_id of the contact
+        @return: The contact data
+        @raises: request.HTTPError, status_code 400, on bad auth credentials
+        @raises: request.HTTPError, status_code 401, on no auth credentials
+        @raises: request.HTTPError, status_code 404, on unknown email_id
+        """
+        resp = self.session.get(f"/ctms/{email_id}")
+        resp.raise_for_status()
+        return resp.json()
+
+    @time_request
+    def put_by_email_id(self, email_id, data):
+        """
+        Call PUT /ctms/{email_id} to replace a CTMS contact by ID
+
+        @param email_id: The CTMS email_id of the contact
+        @param data: The new or updated contact data
+        @return: The created or replaced contact data
+        """
+        resp = self.session.put(f"/ctms/{email_id}", json=data)
+        resp.raise_for_status()
+        return resp.json()
+
+    def put(self, data):
+        """
+        Call PUT /ctms/{email_id} to replace a CTMS contact, using email_id from data
+
+        @return: The created or replaced contact data
+        """
+        email_id = data["email"]["email_id"]
+        return self.put_by_email_id(email_id, data)
+
+
 def ctms_session():
     """Return a CTMSSession configured from Django settings."""
     if settings.CTMS_ENABLED:
@@ -118,5 +236,14 @@ def ctms_session():
             client_id=settings.CTMS_CLIENT_ID,
             client_secret=settings.CTMS_CLIENT_SECRET,
         )
+    else:
+        return None
+
+
+def ctms_interface():
+    """Return a CTMSInterface configured from Django settings."""
+    session = ctms_session()
+    if session:
+        return CTMSInterface(session)
     else:
         return None
