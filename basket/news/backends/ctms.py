ac9fcef55347b7f51c20f23a8e08c6182c6faed3
John Whitlock <jwhitlock@mozilla.com>
2021-04-02 09:51:38 -0500

    Add CTMS.get that returns basket format
    Emulate SFDC.get() and return CTMS contacts in the flat basket format.

--- basket/news/backends/ctms.py
+++ basket/news/backends/ctms.py
@@ -16,6 +16,90 @@ from basket.news.backends.common import get_timer_decorator
 
 time_request = get_timer_decorator("news.backends.ctms")
 
+# Map CTMS group / name pairs to basket flat format names
+# Missing basket names from SFDC integration:
+#  record_type
+#  postal_code
+#  source_url
+#  fsa_*
+#  cv_*
+#  amo_deleted
+#  payee_id
+#  fxa_last_login
+CTMS_TO_BASKET_NAMES = {
+    "amo": {
+        "add_on_ids": None,
+        "display_name": "amo_display_name",
+        "email_opt_in": None,
+        "language": None,
+        "last_login": "amo_last_login",
+        "location": "amo_location",
+        "profile_url": "amo_homepage",
+        "user": "amo_user",
+        "user_id": "amo_id",
+        "username": None,
+        "create_timestamp": None,
+        "update_timestamp": None,
+    },
+    "email": {
+        "primary_email": "email",
+        "basket_token": "token",
+        "double_opt_in": "optin",
+        "sfdc_id": "id",
+        "first_name": "first_name",
+        "last_name": "last_name",
+        "mailing_country": "country",
+        "email_format": "format",
+        "email_lang": "lang",
+        "has_opted_out_of_email": "optout",
+        "unsubscribe_reason": "reason",
+        "email_id": "email_id",
+        "create_timestamp": "created_date",
+        "update_timestamp": "last_modified_date",
+    },
+    "fxa": {
+        "fxa_id": "fxa_id",
+        "primary_email": "fxa_primary_email",
+        "created_date": "fxa_create_date",
+        "lang": "fxa_lang",
+        "first_service": "fxa_service",
+        "account_deleted": "fxa_deleted",
+    },
+    "mofo": {
+        # Need more information about the future MoFo integrations
+        "mofo_email_id": None,
+        "mofo_contact_id": None,
+        "mofo_relevant": None,
+    },
+    "vpn_waitlist": {"geo": "fpn_country", "platform": "fpn_platform"},
+}
+
+
+def from_vendor(contact):
+    """Convert CTMS nested data to basket key-value format
+
+    @params contact: CTMS data
+    @return: dict in basket format
+    """
+    data = {}
+    for group_name, group in contact.items():
+        basket_group = CTMS_TO_BASKET_NAMES.get(group_name)
+        if basket_group:
+            for ctms_name, basket_name in basket_group.items():
+                if basket_name and ctms_name in group:
+                    data[basket_name] = group[ctms_name]
+        elif group_name == "newsletters":
+            # Import newsletter names
+            # Unimported per-newsletter data: format, language, source, unsub_reason
+            newsletters = []
+            for newsletter in group:
+                if newsletter["subscribed"]:
+                    newsletters.append(newsletter["name"])
+            data["newsletters"] = newsletters
+        else:
+            pass
+    return data
+
 
 class CTMSSession:
     """Add authentication to requests to the CTMS API"""
@@ -228,6 +312,69 @@ class CTMSInterface:
         return self.put_by_email_id(email_id, data)
 
 
+class CTMS:
+    """Basket interface to CTMS"""
+
+    def __init__(self, interface):
+        self.interface = interface
+
+    def get(
+        self,
+        email_id=None,
+        token=None,
+        email=None,
+        sfdc_id=None,
+        fxa_id=None,
+        mofo_email_id=None,
+        amo_id=None,
+    ):
+        """
+        Get a contact record, using the first ID provided.
+
+        @param email_id: CTMS email ID
+        @param token: external ID
+        @param email: email address
+        @param sfdc_id: legacy SFDC ID
+        @param fxa_id: external ID from FxA
+        @param mofo_email_id: external ID from MoFo
+        @param amo_id: external ID from AMO
+        @return: dict, or None if disabled
+        """
+        if not self.interface:
+            return None
+
+        if email_id:
+            contact = self.interface.get_by_email_id(email_id)
+        else:
+            if token:
+                params = {"basket_token": token}
+            elif email:
+                params = {"primary_email": email}
+            elif sfdc_id:
+                params = {"sfdc_id": sfdc_id}
+            elif fxa_id:
+                params = {"fxa_id": fxa_id}
+            elif mofo_email_id:
+                params = {"mofo_email_id": mofo_email_id}
+            elif amo_id:
+                params = {"amo_user_id": amo_id}
+            else:
+                raise RuntimeError("One of the arguments must be set")
+
+            contacts = self.interface.get_by_alternate_id(**params)
+            if not contacts:
+                contact = None
+            elif len(contacts) == 1:
+                contact = contacts[0]
+            else:
+                raise RuntimeError(f"Multiple contacts returned for {params}")
+
+        if contact:
+            return from_vendor(contact)
+        else:
+            return None
+
+
 def ctms_session():
     """Return a CTMSSession configured from Django settings."""
     if settings.CTMS_ENABLED:
@@ -247,3 +394,6 @@ def ctms_interface():
         return CTMSInterface(session)
     else:
         return None
+
+
+ctms = CTMS(ctms_interface())
