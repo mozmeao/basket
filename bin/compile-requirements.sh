4cd7de994e319fd0807177802cd9ac1f950bb8b3
Rob Hudson <robhudson@mozilla.com>
2023-04-21 07:59:20 -0700

    Bump pip and pip-tools
    pip==23.1 has a much improved conflict resolver, among other improvements.

--- bin/compile-requirements.sh
+++ bin/compile-requirements.sh
@@ -11,8 +11,8 @@ export CUSTOM_COMPILE_COMMAND="make compile-requirements"
 # We need this installed, but we don't want it to live in the main requirements
 # We will need to periodically review this pinning
 
-pip install -U pip==23.0
-pip install pip-tools==6.12.2
+pip install -U pip==23.1
+pip install pip-tools==6.13.0
 
 pip-compile --generate-hashes -r requirements/prod.in --resolver=backtracking
 pip-compile --generate-hashes -r requirements/dev.in --resolver=backtracking
