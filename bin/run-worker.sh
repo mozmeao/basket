d6ea342b04a83acd935d21f9ec6bc30682f64184
Rob Hudson <robhudson@mozilla.com>
2023-10-25 09:35:10 -0700

    Add --max-jobs to rqworker
    This is so we can cycle the worker pods every so often.

--- bin/run-worker.sh
+++ bin/run-worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 
 NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program \
-python manage.py rqworker --with-scheduler
+python manage.py rqworker --max-jobs "${RQ_MAX_JOBS:-5000}" --with-scheduler
