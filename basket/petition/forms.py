ccca901d0bdadf0dead648d04a40a9e579064bf3
Rob Hudson <robhudson@mozilla.com>
2023-11-07 07:47:17 -0800

    Add petition import management command

--- basket/petition/forms.py
+++ basket/petition/forms.py
@@ -4,6 +4,9 @@ from basket.petition.models import Petition
 
 
 class PetitionForm(forms.ModelForm):
+    title = forms.CharField(required=True)
+    affiliation = forms.CharField(required=True)
+
     class Meta:
         model = Petition
         fields = ["name", "email", "title", "affiliation"]
