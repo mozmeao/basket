af52f9758e95997d8fdd02f13aac01ea4c6b77e3
Paul McLanahan <pmac@mozilla.com>
2023-12-15 13:33:33 -0500

    Add max-requests for gunicorn

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -5,6 +5,7 @@ NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program \
 gunicorn basket.wsgi --bind "0.0.0.0:${PORT:-8000}" \
                      --workers "${WSGI_NUM_WORKERS:-8}" \
                      --worker-class "${WSGI_WORKER_CLASS:-meinheld.gmeinheld.MeinheldWorker}" \
+                     --max-requests "${WSGI_MAX_REQUESTS:-1000}" \
                      --log-level "${WSGI_LOG_LEVEL:-info}" \
                      --error-logfile - \
                      --access-logfile -
