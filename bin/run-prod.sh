2a482f5adfa06dfc4028a05a42e2fd5912a2dc82
Rob Hudson <robhudson@mozilla.com>
2024-01-12 09:48:43 -0800

    uWSGI tuning

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -2,4 +2,4 @@
 
 echo "$GIT_SHA" > static/revision.txt
 NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program \
-    uwsgi --ini /app/bin/uwsgi.ini --workers "${WSGI_NUM_WORKERS:-8}"
+    uwsgi --ini /app/bin/uwsgi.ini
