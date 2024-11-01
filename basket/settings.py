b57f0738ef5b2212b6e0e52b354bc2b899e697d2
Rob Hudson <robhudson@mozilla.com>
2024-11-01 10:12:50 -0700

    Upgrade to django-ratelimit==4.1.0

--- basket/settings.py
+++ basket/settings.py
@@ -162,7 +162,7 @@ MIDDLEWARE = (
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "basket.base.middleware.MetricsViewTimingMiddleware",
-    "ratelimit.middleware.RatelimitMiddleware",
+    "django_ratelimit.middleware.RatelimitMiddleware",
 )
 
 ROOT_URLCONF = "basket.urls"
