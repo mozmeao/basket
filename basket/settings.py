4e38c2bc74bb716532566200c670bf1ce7c0ecab
Rob Hudson <robhudson@mozilla.com>
2024-12-12 09:32:36 -0800

    Migrate to everett
    This is for consistency across meao projects (bedrock, et al)

--- basket/settings.py
+++ basket/settings.py
@@ -10,12 +10,18 @@ import dj_database_url
 import django_cache_url
 import markus
 import sentry_sdk
-from decouple import Csv, UndefinedValueError, config
+from everett.manager import ChoiceOf, ConfigManager, ConfigurationMissingError, ListOf
 from sentry_processor import DesensitizationProcessor
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.logging import ignore_logger
 from sentry_sdk.integrations.redis import RedisIntegration
 from sentry_sdk.integrations.rq import RqIntegration
+
+# The `basic_config` manager searches in this order:
+#   1. environment variables
+#   2. .env file
+#   3. `default` keyword argument
+config = ConfigManager.basic_config()
 
 # Application version.
 VERSION = (0, 1)
@@ -29,9 +35,9 @@ def path(*args):
     return str(ROOT_PATH.joinpath(*args))
 
 
-LOCAL_DEV = config("LOCAL_DEV", False, cast=bool)
-DEBUG = config("DEBUG", default=False, cast=bool)
-UNITTEST = config("UNITTEST", default=False, cast=bool)
+LOCAL_DEV = config("LOCAL_DEV", parser=bool, default="false")
+DEBUG = config("DEBUG", parser=bool, default="false")
+UNITTEST = config("UNITTEST", parser=bool, default="false")
 
 # If we forget to set a `UNITTEST` env var by are running `pytest`, set it.
 if sys.argv[0].endswith(("py.test", "pytest")):
@@ -47,12 +53,9 @@ TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
 # Production uses MySQL, but Sqlite should be sufficient for local development.
 # Our CI server tests against MySQL.
+db_default_url = config("DATABASE_URL", default="sqlite:///basket.db")
 DATABASES = {
-    "default": config(
-        "DATABASE_URL",
-        default="sqlite:///basket.db",
-        cast=dj_database_url.parse,
-    ),
+    "default": dj_database_url.parse(db_default_url),
 }
 if DATABASES["default"]["ENGINE"] == "django.db.backends.mysql":
     DATABASES["default"]["OPTIONS"] = {
@@ -61,16 +64,16 @@ if DATABASES["default"]["ENGINE"] == "django.db.backends.mysql":
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 # CACHE_URL and RQ_URL are derived from REDIS_URL.
-REDIS_URL = config("REDIS_URL", None)
+REDIS_URL = config("REDIS_URL", default="")
 if REDIS_URL:
     REDIS_URL = REDIS_URL.rstrip("/0")
     # Use Redis for cache and rq.
     # Note: We save the URL in the environment so `config` can pull from it below.
-    os.environ["CACHE_URL"] = f'{REDIS_URL}/{config("REDIS_CACHE_DB", "1")}'
-    RQ_URL = f'{REDIS_URL}/{config("REDIS_RQ_DB", "2")}'
+    os.environ["CACHE_URL"] = f'{REDIS_URL}/{config("REDIS_CACHE_DB", default="1")}'
+    RQ_URL = f'{REDIS_URL}/{config("REDIS_RQ_DB", default="2")}'
 
 CACHES = {
-    "default": config("CACHE_URL", default="locmem://", cast=django_cache_url.parse),
+    "default": config("CACHE_URL", parser=django_cache_url.parse, default="locmem://"),
     "bad_message_ids": {
         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
         "TIMEOUT": 12 * 60 * 60,  # 12 hours
@@ -85,26 +88,27 @@ CACHES = {
 default_email_backend = "django.core.mail.backends.console.EmailBackend" if DEBUG else "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_BACKEND = config("EMAIL_BACKEND", default=default_email_backend)
 EMAIL_HOST = config("EMAIL_HOST", default="localhost")
-EMAIL_PORT = config("EMAIL_PORT", default=25, cast=int)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=False, cast=bool)
+EMAIL_PORT = config("EMAIL_PORT", parser=int, default="25")
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", parser=bool, default="false")
 EMAIL_SUBJECT_PREFIX = config("EMAIL_SUBJECT_PREFIX", default="[basket] ")
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
-    default=".allizom.org, .moz.works, basket.mozmar.org, basket.mozilla.com, basket.mozilla.org",
-    cast=Csv(),
+    parser=ListOf(str, allow_empty=False),
+    default="basket-dev.allizom.org,basket.allizom.org,basket.mozilla.org",
 )
-ALLOWED_CIDR_NETS = config("ALLOWED_CIDR_NETS", default="", cast=Csv())
+ALLOWED_CIDR_NETS = config(
+    "ALLOWED_CIDR_NETS",
+    parser=ListOf(str, allow_empty=False),
+    default="",
+)
 USE_X_FORWARDED_HOST = True
 
-SESSION_COOKIE_SECURE = config("SESSION_COOKIE_SECURE", not DEBUG, cast=bool)
-SESSION_ENGINE = config(
-    "SESSION_ENGINE",
-    default="django.contrib.sessions.backends.cache",
-)
-CSRF_COOKIE_SECURE = config("CSRF_COOKIE_SECURE", not DEBUG, cast=bool)
+SESSION_COOKIE_SECURE = config("SESSION_COOKIE_SECURE", parser=bool, default=str(not DEBUG))
+SESSION_ENGINE = config("SESSION_ENGINE", default="django.contrib.sessions.backends.cache")
+CSRF_COOKIE_SECURE = config("CSRF_COOKIE_SECURE", parser=bool, default=str(not DEBUG))
 
 TIME_ZONE = "UTC"
 USE_TZ = True
@@ -129,8 +133,8 @@ if DEBUG:
 try:
     # Make this unique, and don't share it with anybody.
     SECRET_KEY = config("SECRET_KEY")
-except UndefinedValueError as exc:
-    raise UndefinedValueError(
+except ConfigurationMissingError as exc:
+    raise ValueError(
         "The SECRET_KEY environment variable is required. Move env-dist to .env if you want the defaults.",
     ) from exc
 
@@ -184,20 +188,16 @@ INSTALLED_APPS = (
 )
 
 # SecurityMiddleware settings
-SECURE_HSTS_SECONDS = config("SECURE_HSTS_SECONDS", default="0", cast=int)
+SECURE_HSTS_SECONDS = config("SECURE_HSTS_SECONDS", parser=int, default="0")
 SECURE_HSTS_INCLUDE_SUBDOMAINS = False
-SECURE_BROWSER_XSS_FILTER = config("SECURE_BROWSER_XSS_FILTER", default=True, cast=bool)
-SECURE_CONTENT_TYPE_NOSNIFF = config(
-    "SECURE_CONTENT_TYPE_NOSNIFF",
-    default=True,
-    cast=bool,
-)
-SECURE_SSL_REDIRECT = config("SECURE_SSL_REDIRECT", default=False, cast=bool)
+SECURE_BROWSER_XSS_FILTER = config("SECURE_BROWSER_XSS_FILTER", parser=bool, default="true")
+SECURE_CONTENT_TYPE_NOSNIFF = config("SECURE_CONTENT_TYPE_NOSNIFF", parser=bool, default="true")
+SECURE_SSL_REDIRECT = config("SECURE_SSL_REDIRECT", parser=bool, default="false")
 SECURE_REDIRECT_EXEMPT = [
     r"^healthz/$",
     r"^readiness/$",
 ]
-if config("USE_SECURE_PROXY_HEADER", default=SECURE_SSL_REDIRECT, cast=bool):
+if config("USE_SECURE_PROXY_HEADER", parser=bool, default=str(SECURE_SSL_REDIRECT)):
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # watchman
@@ -207,23 +207,11 @@ WATCHMAN_CHECKS = (
     "watchman.checks.databases",
 )
 
+# Send confirmation messages
+SEND_CONFIRM_MESSAGES = config("SEND_CONFIRM_MESSAGES", parser=bool, default="false")
 
-ACOUSTIC_CLIENT_ID = config("ACOUSTIC_CLIENT_ID", None)
-ACOUSTIC_CLIENT_SECRET = config("ACOUSTIC_CLIENT_SECRET", None)
-ACOUSTIC_REFRESH_TOKEN = config("ACOUSTIC_REFRESH_TOKEN", None)
-ACOUSTIC_SERVER_NUMBER = config("ACOUSTIC_SERVER_NUMBER", None)
-ACOUSTIC_FXA_TABLE_ID = config("ACOUSTIC_FXA_TABLE_ID", None)
-ACOUSTIC_FXA_LOG_ENABLED = config("ACOUSTIC_FXA_LOG_ENABLED", True, cast=bool)
-
-ACOUSTIC_TX_CLIENT_ID = config("ACOUSTIC_TX_CLIENT_ID", None)
-ACOUSTIC_TX_CLIENT_SECRET = config("ACOUSTIC_TX_CLIENT_SECRET", None)
-ACOUSTIC_TX_REFRESH_TOKEN = config("ACOUSTIC_TX_REFRESH_TOKEN", None)
-ACOUSTIC_TX_SERVER_NUMBER = config("ACOUSTIC_TX_SERVER_NUMBER", None)
-# Send confirmation messages via Acoustic Transact
-SEND_CONFIRM_MESSAGES = config("SEND_CONFIRM_MESSAGES", False, cast=bool)
-
-BRAZE_API_KEY = config("BRAZE_API_KEY", None)
-BRAZE_BASE_API_URL = config("BRAZE_BASE_API_URL", "https://rest.iad-05.braze.com")
+BRAZE_API_KEY = config("BRAZE_API_KEY", default="")
+BRAZE_BASE_API_URL = config("BRAZE_BASE_API_URL", default="https://rest.iad-05.braze.com")
 # Map of Braze message IDs to the actual message IDs.
 # This is intended for older messages that are hard to change.
 BRAZE_MESSAGE_ID_MAP = {
@@ -232,17 +220,17 @@ BRAZE_MESSAGE_ID_MAP = {
 }
 
 # Mozilla CTMS
-CTMS_ENV = config("CTMS_ENV", "").lower()
-CTMS_ENABLED = config("CTMS_ENABLED", False, cast=bool)
+CTMS_ENV = config("CTMS_ENV", default="").lower()
+CTMS_ENABLED = config("CTMS_ENABLED", parser=bool, default="false")
 if CTMS_ENV == "stage":
     default_url = "https://ctms.stage.mozilla-ess.mozit.cloud"
 elif CTMS_ENV == "prod":
     default_url = "https://ctms.prod.mozilla-ess.mozit.cloud"
 else:
     default_url = ""
-CTMS_URL = config("CTMS_URL", default_url)
-CTMS_CLIENT_ID = config("CTMS_CLIENT_ID", None)
-CTMS_CLIENT_SECRET = config("CTMS_CLIENT_SECRET", None)
+CTMS_URL = config("CTMS_URL", default=default_url)
+CTMS_CLIENT_ID = config("CTMS_CLIENT_ID", default="")
+CTMS_CLIENT_SECRET = config("CTMS_CLIENT_SECRET", default="")
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_URLS_REGEX = r"^/(news/|subscribe)"
@@ -251,13 +239,13 @@ CORS_URLS_REGEX = r"^/(news/|subscribe)"
 RATELIMIT_VIEW = "basket.news.views.ratelimited"
 
 # RQ configuration.
-RQ_RESULT_TTL = config("RQ_RESULT_TTL", default=0, cast=int)  # Ignore results.
-RQ_MAX_RETRY_DELAY = config("RQ_MAX_RETRY_DELAY", default=34 * 60 * 60, cast=int)  # 34 hours in seconds.
-RQ_MAX_RETRIES = 0 if UNITTEST else config("RQ_MAX_RETRIES", default=12, cast=int)
+RQ_RESULT_TTL = config("RQ_RESULT_TTL", parser=int, default="0")  # Ignore results.
+RQ_MAX_RETRY_DELAY = config("RQ_MAX_RETRY_DELAY", parser=int, default=str(34 * 60 * 60))  # 34 hours in seconds.
+RQ_MAX_RETRIES = 0 if UNITTEST else config("RQ_MAX_RETRIES", parser=int, default="12")
 RQ_EXCEPTION_HANDLERS = ["basket.base.rq.store_task_exception_handler"]
-RQ_IS_ASYNC = False if UNITTEST else config("RQ_IS_ASYNC", default=True, cast=bool)
+RQ_IS_ASYNC = False if UNITTEST else config("RQ_IS_ASYNC", parser=bool, default="true")
 
-SNITCH_ID = config("SNITCH_ID", None)
+SNITCH_ID = config("SNITCH_ID", default="")
 
 
 # via http://stackoverflow.com/a/6556951/107114
@@ -276,9 +264,9 @@ def get_default_gateway_linux():
 
 
 HOSTNAME = platform.node()
-CLUSTER_NAME = config("CLUSTER_NAME", default=None)
-K8S_NAMESPACE = config("K8S_NAMESPACE", default=None)
-K8S_POD_NAME = config("K8S_POD_NAME", default=None)
+CLUSTER_NAME = config("CLUSTER_NAME", default="")
+K8S_NAMESPACE = config("K8S_NAMESPACE", default="")
+K8S_POD_NAME = config("K8S_POD_NAME", default="")
 
 # Data scrubbing before Sentry
 # https://github.com/laiyongtao/sentry-processor
@@ -328,16 +316,16 @@ def before_send(event, hint):
 
 if not UNITTEST:
     sentry_sdk.init(
-        dsn=config("SENTRY_DSN", None),
-        release=config("GIT_SHA", None),
+        dsn=config("SENTRY_DSN", default=""),
+        release=config("GIT_SHA", default=""),
         server_name=".".join(x for x in [K8S_NAMESPACE, CLUSTER_NAME, HOSTNAME] if x),
         integrations=[DjangoIntegration(signals_spans=False), RedisIntegration(), RqIntegration()],
         before_send=before_send,
     )
 
-STATSD_HOST = config("STATSD_HOST", get_default_gateway_linux())
-STATSD_PORT = config("STATSD_PORT", 8125, cast=int)
-STATSD_PREFIX = config("STATSD_PREFIX", K8S_NAMESPACE)
+STATSD_HOST = config("STATSD_HOST", default=get_default_gateway_linux())
+STATSD_PORT = config("STATSD_PORT", parser=int, default="8125")
+STATSD_PREFIX = config("STATSD_PREFIX", default=K8S_NAMESPACE)
 
 if LOCAL_DEV:
     MARKUS_BACKENDS = [
@@ -357,11 +345,19 @@ else:
 
 markus.configure(backends=MARKUS_BACKENDS)
 
+LOG_LEVEL = config(
+    "DJANGO_LOG_LEVEL",
+    parser=ChoiceOf(
+        str,
+        ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+    ),
+    default="WARNING",
+)
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "root": {
-        "level": config("DJANGO_LOG_LEVEL", default="WARNING"),
+        "level": LOG_LEVEL,
         "handlers": ["console"],
     },
     "formatters": {
@@ -396,36 +392,36 @@ PROD_DETAILS_CACHE_TIMEOUT = None
 
 # language codes that we support and send through to backend
 # regardless of their existence in the DB
-EXTRA_SUPPORTED_LANGS = config("EXTRA_SUPPORTED_LANGS", "", cast=Csv())
+EXTRA_SUPPORTED_LANGS = config(
+    "EXTRA_SUPPORTED_LANGS",
+    parser=ListOf(str, allow_empty=False),
+    default="",
+)
 
 if UNITTEST:
     TESTING_EMAIL_DOMAINS = []
 else:
     TESTING_EMAIL_DOMAINS = config(
         "TESTING_EMAIL_DOMAINS",
-        "restmail.net,restmail.lcip.org,example.com",
-        cast=Csv(),
+        parser=ListOf(str, allow_empty=False),
+        default="restmail.net,example.com",
     )
 
 # FIXME: MAINTENANCE_MODE is considered broken and needs to be fixed before use.
-MAINTENANCE_MODE = config("MAINTENANCE_MODE", False, cast=bool)
-QUEUE_BATCH_SIZE = config("QUEUE_BATCH_SIZE", 500, cast=int)
+MAINTENANCE_MODE = config("MAINTENANCE_MODE", parser=bool, default="false")
+QUEUE_BATCH_SIZE = config("QUEUE_BATCH_SIZE", parser=int, default="500")
 # can we read user data in maintenance mode
-MAINTENANCE_READ_ONLY = config("MAINTENANCE_READ_ONLY", False, cast=bool)
+MAINTENANCE_READ_ONLY = config("MAINTENANCE_READ_ONLY", parser=bool, default="false")
 
-USE_SANDBOX_BACKEND = config("USE_SANDBOX_BACKEND", False, cast=bool)
+USE_SANDBOX_BACKEND = config("USE_SANDBOX_BACKEND", parser=bool, default="false")
 
-FXA_EVENTS_QUEUE_ENABLE = config("FXA_EVENTS_QUEUE_ENABLE", cast=bool, default=False)
-FXA_EVENTS_QUEUE_IGNORE_MODE = config(
-    "FXA_EVENTS_QUEUE_IGNORE_MODE",
-    cast=bool,
-    default=False,
-)
+FXA_EVENTS_QUEUE_ENABLE = config("FXA_EVENTS_QUEUE_ENABLE", parser=bool, default="false")
+FXA_EVENTS_QUEUE_IGNORE_MODE = config("FXA_EVENTS_QUEUE_IGNORE_MODE", parser=bool, default="false")
 FXA_EVENTS_ACCESS_KEY_ID = config("FXA_EVENTS_ACCESS_KEY_ID", default="")
 FXA_EVENTS_SECRET_ACCESS_KEY = config("FXA_EVENTS_SECRET_ACCESS_KEY", default="")
 FXA_EVENTS_QUEUE_REGION = config("FXA_EVENTS_QUEUE_REGION", default="")
 FXA_EVENTS_QUEUE_URL = config("FXA_EVENTS_QUEUE_URL", default="")
-FXA_EVENTS_QUEUE_WAIT_TIME = config("FXA_EVENTS_QUEUE_WAIT_TIME", cast=int, default=10)
+FXA_EVENTS_QUEUE_WAIT_TIME = config("FXA_EVENTS_QUEUE_WAIT_TIME", parser=int, default="10")
 FXA_EVENTS_SNITCH_ID = config("FXA_EVENTS_SNITCH_ID", default="")
 
 # stage or production
@@ -433,17 +429,11 @@ FXA_EVENTS_SNITCH_ID = config("FXA_EVENTS_SNITCH_ID", default="")
 FXA_OAUTH_SERVER_ENV = config("FXA_OAUTH_SERVER_ENV", default="production")
 FXA_CLIENT_ID = config("FXA_CLIENT_ID", default="")
 FXA_CLIENT_SECRET = config("FXA_CLIENT_SECRET", default="")
-FXA_OAUTH_TOKEN_TTL = config("FXA_OAUTH_TOKEN_TTL", default=300, cast=int)  # 5 minutes
+FXA_OAUTH_TOKEN_TTL = config("FXA_OAUTH_TOKEN_TTL", parser=int, default="300")  # 5 minutes
 
 FXA_EMAIL_PREFS_DOMAIN = config("FXA_EMAIL_PREFS_DOMAIN", default="www.mozilla.org")
-FXA_REGISTER_NEWSLETTER = config(
-    "FXA_REGISTER_NEWSLETTER",
-    default="firefox-accounts-journey",
-)
-FXA_REGISTER_SOURCE_URL = config(
-    "FXA_REGISTER_SOURCE_URL",
-    default="https://accounts.firefox.com/",
-)
+FXA_REGISTER_NEWSLETTER = config("FXA_REGISTER_NEWSLETTER", default="firefox-accounts-journey")
+FXA_REGISTER_SOURCE_URL = config("FXA_REGISTER_SOURCE_URL", default="https://accounts.firefox.com/")
 # TODO move this to the DB
 FXA_LOGIN_CAMPAIGNS = {
     "fxa-embedded-form-moz": "mozilla-welcome",
@@ -456,16 +446,16 @@ FXA_LOGIN_CAMPAIGNS = {
 
 COMMON_VOICE_NEWSLETTER = config("COMMON_VOICE_NEWSLETTER", default="common-voice")
 
-OIDC_ENABLE = config("OIDC_ENABLE", default=False, cast=bool)
+OIDC_ENABLE = config("OIDC_ENABLE", parser=bool, default="false")
 if OIDC_ENABLE:
     AUTHENTICATION_BACKENDS = ("basket.base.authentication.OIDCModelBackend",)
-    OIDC_OP_AUTHORIZATION_ENDPOINT = config("OIDC_OP_AUTHORIZATION_ENDPOINT")
-    OIDC_OP_TOKEN_ENDPOINT = config("OIDC_OP_TOKEN_ENDPOINT")
-    OIDC_OP_USER_ENDPOINT = config("OIDC_OP_USER_ENDPOINT")
+    OIDC_OP_AUTHORIZATION_ENDPOINT = config("OIDC_OP_AUTHORIZATION_ENDPOINT", default="")
+    OIDC_OP_TOKEN_ENDPOINT = config("OIDC_OP_TOKEN_ENDPOINT", default="")
+    OIDC_OP_USER_ENDPOINT = config("OIDC_OP_USER_ENDPOINT", default="")
 
-    OIDC_RP_CLIENT_ID = config("OIDC_RP_CLIENT_ID")
-    OIDC_RP_CLIENT_SECRET = config("OIDC_RP_CLIENT_SECRET")
-    OIDC_CREATE_USER = config("OIDC_CREATE_USER", default=False, cast=bool)
+    OIDC_RP_CLIENT_ID = config("OIDC_RP_CLIENT_ID", default="")
+    OIDC_RP_CLIENT_SECRET = config("OIDC_RP_CLIENT_SECRET", default="")
+    OIDC_CREATE_USER = config("OIDC_CREATE_USER", parser=bool, default="false")
     LOGIN_REDIRECT_URL = "/admin/"
     OIDC_EXEMPT_URLS = [
         "/",
@@ -481,8 +471,3 @@ if OIDC_ENABLE:
         "/readiness/",
         "/watchman/",
     ]
-
-PETITION_CORS_URL = config("PETITION_CORS_URL", default="https://open.mozilla.org")
-PETITION_LETTER_URL = config("PETITION_LETTER_URL", default="https://open.mozilla.org/letter")
-PETITION_THANKS_URL = config("PETITION_THANKS_URL", default="https://open.mozilla.org/letter/thanks")
-PETITION_BUILD_HOOK_URL = config("PETITION_BUILD_HOOK_URL", default=None)
