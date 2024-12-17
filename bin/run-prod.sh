a316eb0633dbfb898b7cb83ad22112e76ce78364
Rob Hudson <robhudson@mozilla.com>
2024-12-17 11:25:08 -0800

    Migrate to granian
    This also drops newrelic since newrelic doesn't support granian.

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -1,5 +1,20 @@
 #!/bin/bash -ex
 
 echo "$GIT_SHA" > static/revision.txt
-NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program \
-    uwsgi --ini /app/bin/uwsgi.ini
+
+# Granian recommends containerized apps use the defaults of workers=1, threads=1.
+# The backgpressure defaults to backlog (1024) / workers (1), but should be adjusted to match the
+# number of database connections when configured for databases.
+
+# Set the various GRANIAN_* environment variables to configure Granian, notably:
+# - GRANIAN_PORT
+# - GRANIAN_WORKERS
+# - GRANIAN_THREADS
+# - GRANIAN_BLOCKING_THREADS
+# - GRANIAN_BACKPRESSURE
+# - GRANIAN_LOG_LEVEL
+granian \
+    --interface wsgi \
+    --host "0.0.0.0" \
+    --no-ws \
+    basket.wsgi:application
