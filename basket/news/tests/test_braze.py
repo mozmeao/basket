af9c96f1a55e54269b26cc1e00e16e13159f2d19
clara-campos <64791123+clara-campos@users.noreply.github.com>
2025-10-27 15:15:07 -0300

    add tests for to_vendor and from_vendor

--- basket/news/tests/test_braze.py
+++ basket/news/tests/test_braze.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from unittest import mock
 
 from django.utils import timezone
@@ -7,6 +8,7 @@ import requests_mock
 from freezegun import freeze_time
 
 from basket.news.backends import braze
+from basket.news.backends.braze import Braze
 
 
 @pytest.fixture
@@ -257,3 +259,279 @@ def test_braze_exception_500(braze_client):
         m.register_uri("POST", "http://test.com/users/track", status_code=500, json={})
         with pytest.raises(braze.BrazeInternalServerError):
             braze_client.track_user("test@test.com")
+
+
+mock_basket_user_data = {
+    "email": "test@example.com",
+    "email_id": "123",
+    "id": "456",
+    "first_name": "Test",
+    "last_name": "User",
+    "country": "US",
+    "lang": "en",
+    "newsletters": ["foo-news"],
+    "created_date": "2022-01-01",
+    "last_modified_date": "2022-02-01",
+    "optin": True,
+    "optout": False,
+    "token": "abc",
+    "fxa_service": "test",
+    "fxa_lang": "en",
+    "fxa_primary_email": "test2@example.com",
+    "fxa_create_date": "2022-01-02",
+}
+
+mock_braze_user_data = {
+    "email": "test@example.com",
+    "external_id": "123",
+    "braze_id": "456",
+    "first_name": "Test",
+    "last_name": "User",
+    "country": "US",
+    "language": "en",
+    "email_subscribe": "opted_in",
+    "custom_attributes": {
+        "user_attributes_v1": [
+            {
+                "mailing_country": "us",
+                "email_lang": "en",
+                "created_at": "2022-01-01",
+                "updated_at": "2022-02-01",
+                "basket_token": "abc",
+                "fxa_first_service": "test",
+                "fxa_lang": "en",
+                "fxa_primary_email": "test2@example.com",
+                "fxa_created_at": "2022-01-02",
+                "has_fxa": True,
+            }
+        ]
+    },
+}
+
+mock_braze_user_subscription_groups = [
+    {"id": "234c9b4a-1785-4cd5-b839-5dbc134982eb", "status": "Subscribed"},
+    {"id": "78fe6671-9f94-48bd-aaf3-7e873536c3e6", "status": "Not Subscribed"},
+]
+
+mock_newsletters = {
+    "by_vendor_id": {
+        "234c9b4a-1785-4cd5-b839-5dbc134982eb": namedtuple("Newsletter", ["slug"])("foo-news"),
+        "78fe6671-9f94-48bd-aaf3-7e873536c3e6": namedtuple("Newsletter", ["slug"])("bar-news"),
+    },
+    "by_name": {
+        "foo-news": namedtuple("Newsletter", ["vendor_id"])("234c9b4a-1785-4cd5-b839-5dbc134982eb"),
+        "bar-news": namedtuple("Newsletter", ["vendor_id"])("78fe6671-9f94-48bd-aaf3-7e873536c3e6"),
+    },
+}
+
+
+@mock.patch(
+    "basket.news.newsletters._newsletters",
+    return_value=mock_newsletters,
+)
+def test_from_vendor(braze_client):
+    braze_instance = Braze(braze_client())
+
+    assert braze_instance.from_vendor(mock_braze_user_data, mock_braze_user_subscription_groups) == mock_basket_user_data
+
+
+@mock.patch(
+    "basket.news.newsletters._newsletters",
+    return_value=mock_newsletters,
+)
+@mock.patch(
+    "basket.news.newsletters.newsletter_languages",
+    return_value=["en"],
+)
+def test_to_vendor_with_user_data_and_no_updates(self, braze_client):
+    braze_instance = Braze(braze_client())
+    dt = timezone.now()
+    expected = {
+        "attributes": [
+            {
+                "_update_existing_only": True,
+                "email": "test@example.com",
+                "external_id": "123",
+                "email_subscribe": "opted_in",
+                "subscription_groups": [],
+                "update_timestamp": dt.isoformat(),
+                "user_attributes_v1": [
+                    {
+                        "mailing_country": "us",
+                        "email_lang": "en",
+                        "created_at": {
+                            "$time": "2022-01-01",
+                        },
+                        "basket_token": "abc",
+                        "fxa_first_service": "test",
+                        "fxa_lang": "en",
+                        "fxa_primary_email": "test2@example.com",
+                        "fxa_created_at": "2022-01-02",
+                        "has_fxa": True,
+                        "updated_at": {
+                            "$time": dt.isoformat(),
+                        },
+                    }
+                ],
+            }
+        ]
+    }
+    with freeze_time(dt):
+        assert braze_instance.to_vendor(mock_basket_user_data) == expected
+
+
+@mock.patch(
+    "basket.news.newsletters._newsletters",
+    return_value=mock_newsletters,
+)
+@mock.patch(
+    "basket.news.newsletters.newsletter_languages",
+    return_value=["en"],
+)
+def test_to_vendor_with_updates_and_no_user_data(self, braze_client):
+    braze_instance = Braze(braze_client())
+    dt = timezone.now()
+    update_data = {"newsletters": {"bar-news": True}, "email": "test@example.com", "token": "abc", "email_id": "123"}
+    expected = {
+        "attributes": [
+            {
+                "_update_existing_only": True,
+                "email": "test@example.com",
+                "external_id": "123",
+                "email_subscribe": "subscribed",
+                "subscription_groups": [
+                    {"subscription_group_id": "78fe6671-9f94-48bd-aaf3-7e873536c3e6", "subscription_state": "subscribed"},
+                ],
+                "update_timestamp": dt.isoformat(),
+                "user_attributes_v1": [
+                    {
+                        "email_lang": "en",
+                        "created_at": {
+                            "$time": dt.isoformat(),
+                        },
+                        "basket_token": "abc",
+                        "fxa_first_service": None,
+                        "fxa_lang": None,
+                        "fxa_primary_email": None,
+                        "fxa_created_at": None,
+                        "has_fxa": False,
+                        "mailing_country": None,
+                        "updated_at": {
+                            "$time": dt.isoformat(),
+                        },
+                    }
+                ],
+            }
+        ]
+    }
+    with freeze_time(dt):
+        assert braze_instance.to_vendor(None, update_data) == expected
+
+
+@mock.patch(
+    "basket.news.newsletters._newsletters",
+    return_value=mock_newsletters,
+)
+@mock.patch(
+    "basket.news.newsletters.newsletter_languages",
+    return_value=["en"],
+)
+def test_to_vendor_with_both_user_data_and_updates(self, braze_client):
+    braze_instance = Braze(braze_client())
+    dt = timezone.now()
+    update_data = {"newsletters": {"bar-news": True, "foo-news": False}, "first_name": "Foo", "country": "CA", "optin": False}
+    expected = {
+        "attributes": [
+            {
+                "_update_existing_only": True,
+                "email": "test@example.com",
+                "external_id": "123",
+                "email_subscribe": "subscribed",
+                "first_name": "Foo",
+                "country": "ca",
+                "subscription_groups": [
+                    {"subscription_group_id": "78fe6671-9f94-48bd-aaf3-7e873536c3e6", "subscription_state": "subscribed"},
+                    {"subscription_group_id": "234c9b4a-1785-4cd5-b839-5dbc134982eb", "subscription_state": "unsubscribed"},
+                ],
+                "update_timestamp": dt.isoformat(),
+                "user_attributes_v1": [
+                    {
+                        "mailing_country": "ca",
+                        "email_lang": "en",
+                        "created_at": {
+                            "$time": "2022-01-01",
+                        },
+                        "basket_token": "abc",
+                        "fxa_first_service": "test",
+                        "fxa_lang": "en",
+                        "fxa_primary_email": "test2@example.com",
+                        "fxa_created_at": "2022-01-02",
+                        "has_fxa": True,
+                        "updated_at": {
+                            "$time": dt.isoformat(),
+                        },
+                    }
+                ],
+            }
+        ]
+    }
+    with freeze_time(dt):
+        assert braze_instance.to_vendor(mock_basket_user_data, update_data) == expected
+
+
+@mock.patch(
+    "basket.news.newsletters._newsletters",
+    return_value=mock_newsletters,
+)
+@mock.patch(
+    "basket.news.newsletters.newsletter_languages",
+    return_value=["en"],
+)
+def test_to_vendor_with_custom_attributes_and_events(self, braze_client):
+    braze_instance = Braze(braze_client())
+    dt = timezone.now()
+    events = [
+        {
+            "name": "test event",
+            "time": dt,
+            "external_id": "123",
+        }
+    ]
+    expected = {
+        "attributes": [
+            {
+                "_update_existing_only": False,
+                "email": "test@example.com",
+                "external_id": "123",
+                "email_subscribe": "opted_in",
+                "subscription_groups": [],
+                "update_timestamp": dt.isoformat(),
+                "user_attributes_v1": [
+                    {
+                        "mailing_country": "us",
+                        "email_lang": "en",
+                        "created_at": {
+                            "$time": "2022-01-01",
+                        },
+                        "basket_token": "abc",
+                        "fxa_first_service": "test",
+                        "fxa_lang": "en",
+                        "fxa_primary_email": "test2@example.com",
+                        "fxa_created_at": "2022-01-02",
+                        "has_fxa": True,
+                        "updated_at": {
+                            "$time": dt.isoformat(),
+                        },
+                    }
+                ],
+            }
+        ],
+        "events": events,
+    }
+    with freeze_time(dt):
+        assert (
+            braze_instance.to_vendor(
+                basket_user_data=mock_basket_user_data, update_data=None, custom_attributes={"_update_existing_only": False}, events=events
+            )
+            == expected
+        )
