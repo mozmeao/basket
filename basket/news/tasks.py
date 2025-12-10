30680f7024fb656339677bb127a662fa352de468
clara-campos <64791123+clara-campos@users.noreply.github.com>
2025-12-10 13:46:28 -0400

    fix conditional

--- basket/news/tasks.py
+++ basket/news/tasks.py
@@ -329,7 +329,7 @@ def upsert_contact(
             return None, None
     elif use_braze_backend and update_data.get("optout"):
         # Unsubscribe from all current Braze newsletters if user is opting out
-        newsletters = cur_newsletters if api_call_type == UNSUBSCRIBE else []
+        newsletters = cur_newsletters if cur_newsletters and api_call_type == UNSUBSCRIBE else []
 
     # Set the newsletter flags in the record by comparing to their
     # current subscriptions.
