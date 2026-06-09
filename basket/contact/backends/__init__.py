539048fc9970a802d09a26000fc451fb79cc0a55
Matthew Semeniuk <msemeniuk@mozilla.com>
2026-06-09 10:20:14 -0700

    ruff format

--- basket/contact/backends/__init__.py
+++ basket/contact/backends/__init__.py
@@ -12,9 +12,7 @@ def get_contact_sink() -> ContactSink:
     key = settings.ENTERPRISE_CONTACT_SINK
     dotted_path = _REGISTRY.get(key)
     if dotted_path is None:
-        raise ImproperlyConfigured(
-            f"Unknown ENTERPRISE_CONTACT_SINK: {key!r}. Valid choices: {list(_REGISTRY)}"
-        )
+        raise ImproperlyConfigured(f"Unknown ENTERPRISE_CONTACT_SINK: {key!r}. Valid choices: {list(_REGISTRY)}")
     from django.utils.module_loading import import_string
 
     return import_string(dotted_path)()
