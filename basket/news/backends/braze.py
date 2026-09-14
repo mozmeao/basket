08a77a48709be860e8c8edef6d6f977181b3cb23
Matthew Semeniuk <9813994+bluewave41@users.noreply.github.com>
2026-09-14 14:13:07 -0300

    Add basket_token alias to lookup (#1768)
    * add basket_token alias for lookups
    
    * Add fxa_id lookup support
    
    * Gate fxa_id lookups and don't double parse UUIDs
    
    * Remove FXA lookup and converter

--- basket/news/backends/braze.py
+++ basket/news/backends/braze.py
@@ -249,6 +249,7 @@ class BrazeInterface:
 
         if external_id:
             data["external_ids"] = [external_id]
+            data["user_aliases"].append({"alias_name": external_id, "alias_label": "basket_token"})
 
         if fields_to_export:
             data["fields_to_export"] = fields_to_export
